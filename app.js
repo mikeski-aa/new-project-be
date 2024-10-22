3fab15daac381169c2a3e6a19a24810471bf80fe
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-10-22 12:40:04 +0200

    Added CORS

--- app.js
+++ app.js
@@ -3,12 +3,21 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
+
+require("dotenv").config();
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
 const apiRouter = require("./routes/apiRoutes");
 
 const app = express();
+
+const corsOptions = {
+  origin: [process.env.LOCAL_URL, process.env.NONLOCAL_URL],
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 app.use(logger("dev"));
 app.use(express.json());

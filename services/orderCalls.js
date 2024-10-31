e272bb3686d47a399bc7325dc5d8ee41a97cf7c8
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-10-31 13:05:28 +0100

    Created db call to create order item

--- services/orderCalls.js
+++ services/orderCalls.js
@@ -15,3 +15,22 @@ async function createOrder(storeid, tval) {
     throw new Error("Error creating an order");
   }
 }
+
+async function createOrderItem(orderId, sku, quantityordered) {
+  try {
+    const response = await prisma.ordereditem.create({
+      data: {
+        orderId: orderId,
+        sku: sku,
+        quantityordered: quantityordered,
+      },
+    });
+
+    return response;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Error creating an order item");
+  }
+}
+
+module.exports = { createOrder, createOrderItem };
